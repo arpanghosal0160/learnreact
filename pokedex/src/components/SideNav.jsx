5f47a88fda4d9e90875b7679fd144189877de17d
Arpan Ghosal <arpanghosal0160@gmail.com>
2025-05-16 23:31:56 +0530

    updated navbar component

--- pokedex/src/components/SideNav.jsx
+++ pokedex/src/components/SideNav.jsx
@@ -1,9 +1,17 @@
-
+import { first151Pokemon, getFullPokedexNumber } from "../utils"
 
 export function SideNav() {
     return(
+        //we are building a nav bar in the app, the function basically loops over all the 151 pokemon that is given in the utils file and the makes a button
+        //for each pokemon. The {getFullPokedexNumber(pokemonIndex)} is used to return the index of the pokemon and the 
+        //{pokemon} in the next line is used to render the name of the pokemon according to the pokedex
         <nav>
-
+            {first151Pokemon.map((pokemon, pokemonIndex) => (
+                <button key={pokemonIndex}>
+                    <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                    <p>{pokemon}</p>
+                </button>
+            ))}
         </nav>
     )
         
