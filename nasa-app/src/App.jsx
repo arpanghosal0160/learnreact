b7fa82c0ba73486f113bf3dc1019b8b4a5502978
Arpan Ghosal <arpanghosal0160@gmail.com>
2025-05-16 09:29:23 +0530

    nasa-app init

---
+++ nasa-app/src/App.jsx
@@ -0,0 +1,18 @@
+import Main from "./components/Main"
+import SideBar from "./components/SideBar"
+import footer from "./components/footer"
+
+
+function App() {
+  
+  return (
+    <>
+      <SideBar />
+      <Main />
+      <footer />
+
+    </>
+  )
+}
+
+export default App
---
+++ nasa-app/src/components/footer.jsx
@@ -0,0 +1,10 @@
+export default function footer() {
+    return(
+        <div>
+            <footer>
+                <p>© 2023 NASA. All rights reserved.</p>
+                <p>Website by [Arpan Ghosal]</p>
+            </footer>
+        </div>
+    )
+}
