7bbd67e71376d69102cf0bc8f92b1eb173d7c07d
Arpan Ghosal <arpanghosal0160@gmail.com>
2025-05-17 08:25:23 +0530

    added head

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -5,15 +5,21 @@ import { SideNav } from './components/SideNav'
 import { PokeCard } from './components/PokeCard'
 import { Header } from './components/header'
 
+//we needthe user to search for an pokemon so that we can find the details. So we need to define the states in the App.jsx file
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedPokemon, setSelectedPokemon] = useState(0) //we initialize the hook at the index 0, ie. bulbasaur
+
 
   return (
     <div>
       <Header />
-      <SideNav />
-      <PokeCard />
+      <SideNav selectedPokemon={selectedPokemon} setSelectedPokemon={setSelectedPokemon}/>
+      <PokeCard selectedPokemon={selectedPokemon}/>
 
     </div>
   )
--- pokedex/src/components/PokeCard.jsx
+++ pokedex/src/components/PokeCard.jsx
@@ -1,4 +1,9 @@
-export function PokeCard(){
+//we need the data from the api in the pokecard. So we need to write the logic of the api fetching in this part of the component
+import { useEffect, useState } from 'react'
+
+export function PokeCard(props){
+    const {selectedPokemon} = props
+    
     return(
         <div>
 
